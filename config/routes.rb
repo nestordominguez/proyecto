7997ad501955f29c92796eb74d57e0562f792134
Nestor Oscar Dominguez Diaz <oscardom80@hotmail.com>
2014-08-17 18:46:10 -0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Proyecto::Application.routes.draw do
   
-  get "statics_pages/help"
-  get "statics_pages/about"
-  get "statics_pages/contact"
+  root 'statics_pages#home'
+  
+  match '/signup',    to: 'users#new',              via: 'get'
+  match '/help',      to: 'statics_pages#help',     via: 'get'
+  match '/about',     to: 'statics_pages#about',    via: 'get'
+  match '/contact',   to: 'statics_pages#contact',  via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'statics_pages#home'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,47 +1,27 @@
 require 'spec_helper'
 
 describe "Statics pages" do
-  let(:title) {'Ruby on Rails Tutorial Sample App |'}
+  subject {page}
 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/'
-      expect(page).to have_content('Sample App')
-    end
-    it "should have the right title 'Home'" do
-    	visit '/'
-    	expect(page).to have_title("#{title} Home")
-    end
+    before { visit root_path }
+    it {should have_content('Sample App')}
+    it {should have_title(full_title("Home"))}
   end
   describe "Help page" do
-    it "should have the content 'Help'" do
-    	visit '/statics_pages/help'
-    	expect(page).to have_content('Help')
-    end
-    it "should have the right title 'Help'" do
-    	visit '/statics_pages/help'
-    	expect(page).to have_title("#{title} Help")
-    end
+    before {visit help_path}    
+    it {should have_content('Help')}
+    it {should have_title(full_title("Help"))}
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-    	visit '/statics_pages/about'
-    	expect(page).to have_content('About Us')
-    end
-    it "should have the right title 'About Us'" do
-    	visit '/statics_pages/about'
-    	expect(page).to have_title("#{title} About Us")
-    end
+    before {visit about_path}
+    it {should have_content('About Us')}
+    it {should have_title(full_title("About Us"))}
   end
-  describe "Contact" do
-    it "should have the content 'Contact'" do
-      visit '/statics_pages/contact'
-      expect(page).to have_content('Contact')
-    end
-    it "should have the right title 'Contact'" do
-      visit '/statics_pages/contact'
-      expect(page).to have_title("#{title} Contact")
-    end
+  describe "Contact page" do
+    before {visit contact_path}
+    it {should have_content('Contact')}
+    it {should have_title(full_title("Contact"))}
   end
 end
